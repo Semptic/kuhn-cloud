4ff8e729a84b6f7f50ee699de3603f522503e46b
Stefan Kuhn <semptic@kuhn.cloud>
2024-01-06 21:40:20 +0000

    Reorganize file

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ set -e
 
 ./with_secrets.sh apply setup/user
 ./with_secrets.sh apply setup/tailscale
-./with_secrets.sh apply setup/monitoring
 
+./with_secrets.sh apply setup/monitoring
 ./with_secrets.sh apply setup/kubernetes-dashboard
+
 ./with_secrets.sh apply setup/longhorn
 ./with_secrets.sh apply setup/smb-storage-box
